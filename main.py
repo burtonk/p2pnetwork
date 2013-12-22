8602ad4a08df1276b62e0a10bc866b80d1e580ec
burtonk <burtonk@tcd.ie>
2013-12-22 23:00:22 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+import thread
+from project import *
+
+def main(): 
+	lol = Input("lol")
+
+if __name__ == "__main__":
+    main()
+
+    #"7": "127.0.0.1","4": "127.0.0.2"
+    #{"node_id": "7", "ip_address": "127.0.0.1"},{"node_id": "4", "ip_address": "127.0.0.2"}
